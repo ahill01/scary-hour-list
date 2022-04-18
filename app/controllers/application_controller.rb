34b1408bf698ac19c0a3e119d7b9fc2ffedcf106
Allison <amh03160@gmail.com>
2022-04-18 14:37:11 -0500

    added routes, application controller so we can access data from backend

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,19 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  get "/users" do
+    User.all.to_json(include: :tasks)
   end
+
+  get "/tasks" do 
+    Task.all
+  end
+
+  get "/users/:id" do 
+    user = User.find(params[:id])
+    user.to_json(include: :tasks)
+  end
+
+ 
 
 end
